d3633170c7d181248d5bbeaeb2479f08ba53ff54
Abby <abigaillyons@college.harvard.edu>
2016-02-11 00:08:37 -0500

    started with features

--- practical 1/enetTests2.py
+++ practical 1/enetTests2.py
@@ -10,11 +10,13 @@ import os
 from sklearn import cross_validation
 from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet, ElasticNetCV, LassoLarsCV
 
-fdefName = os.path.join(RDConfig.RDDataDir,’BaseFeatures.fdef’)
+fdefName = os.path.join(RDConfig.RDDataDir,'BaseFeatures.fdef')
 factory = ChemicalFeatures.BuildFeatureFactory(fdefName)
 
 df_train = pd.read_csv("train.csv")
+df_train = df_train.head(1000)
 df_test = pd.read_csv("test.csv")
+df_test = df_test.head(1000)
 
 def write_to_file(filename, predictions):
     with open(filename, "w") as f:
@@ -24,17 +26,30 @@ def write_to_file(filename, predictions):
 
 def getfeat(smile_array):
     feats = []
+    featIds = []
+    min = 6
+    maxId = 0
     for smile in smile_array:
         m = Chem.MolFromSmiles(smile)
-        feats.append(factory.GetFeaturesForMol(m))
-    return np.vstack(feats)
+        f = factory.GetFeaturesForMol(m)
+        #if (len(f) < min): min = len(f)
+        feats.append(f[:min])
+    for m in range(0, len(feats)):
+    	for n in range(0, len(feats[m])):
+    		if (feats[m][n].GetId() > maxId): maxId = feats[m][n].GetId()
+    	#feats[m] = feats[m][:min]
+    print(maxId)
+    return np.vstack(featIds)
 
 print("Loading data :)")
 #store gap values
 Y_train = df_train.gap.values
+print(Y_train)
+print(Y_train.shape)
 X_test = getfeat(df_test.smiles.values)
 base_X = getfeat(df_train.smiles.values)
 base_Y = np.vstack(Y_train)
+print(base_Y)
 
 
 def ridgereg(a):
@@ -65,6 +80,8 @@ def enetCV():
     print ("Doing elastic net")
     cross_val = cross_validation.ShuffleSplit(len(base_X), n_iter=5, test_size=0.2, random_state=0)
     clf4 = ElasticNetCV(cv=cross_val)
+    print(base_X.shape)
+    print(base_Y.shape)
     clf4.fit(base_X, base_Y)
     print ("Score = %f" % clf4.score(base_X, base_Y))
     clf4_pred = clf4.predict(X_test)
@@ -81,24 +98,3 @@ def lassolarscv():
 
 enetCV()
 lassolarscv()
-
-# LASSO/ENET PATH CALCULATIONS
-# Not sure how to make predictions with this...
-
-# eps = 5e-3
-
-# print("Computing lasso")
-# alphas_lasso, coefs_lasso, _ = lasso_path(
-# 	base_X, base_Y, eps, fit_intercept=False)
-
-# print("Computing positive lasso")
-# alphas_positive_lasso, coefs_positive_lasso, _ = lasso_path(
-# 	base_X, base_Y, eps, positive=True, fit_intercept=False)
-
-# print("Computing elastic net")
-# alphas_enet, coefs_enet, _ = enet_path(
-# 	base_X, base_Y, eps=eps, l1_ratio=0.8, fit_intercept=False)
-
-# print("Computing positive elastic net")
-# alphas_positive_enet, coefs_positive_enet, _ = enet_path(
-# 	base_X, base_Y, eps=eps, l1_ratio=0.8, positive=True, fit_intercept=False)

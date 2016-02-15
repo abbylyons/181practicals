dc67efa5e8212ce43fd8b4d14a6137534abbc13e
Timothy tamm <tamm@college.harvard.edu>
2016-02-15 15:11:47 -0500

    implemented priorityqueue and init

--- practical 1/Neural net approach/config.py
+++
@@ -1,6 +0,0 @@
-__author__ = 'timothy'
-
-
-NPL = 250
-inputs = 256
-outputs = 1
--- practical 1/enetTests2.py
+++ practical 1/enetTests2.py
@@ -14,12 +14,11 @@ from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet, Ela
 fdefName = os.path.join(RDConfig.RDDataDir,'BaseFeatures.fdef')
 factory = ChemicalFeatures.BuildFeatureFactory(fdefName)
 
-fpl = 1024
+fpl = 512
 
-df_train = pd.read_csv("train.csv")
-df_train = df_train.head(100)
-df_test = pd.read_csv("test.csv")
-df_test = df_test.head(100)
+df_train_new = pd.read_csv("base_X2.csv")
+df_y_train = pd.read_csv("y_train.csv")
+
 
 def write_to_file(filename, predictions):
     with open(filename, "w") as f:
@@ -29,15 +28,16 @@ def write_to_file(filename, predictions):
 
 
 def write_to_inp_file(filename, table):
+    print("writing " + filename)
     with open(filename, "w") as f:
-        f.write("bit0")
-        for i in range(1,fpl):
-            f.write(", bit" + str(i))
+        f.write("gap")
+        # for i in range(1,fpl):
+        #     f.write(", bit" + str(i))
         f.write("\n")
         for row in table:
-            for el in row[:-1]:
-                f.write(str(el) + ", ")
-            f.write(str(el)+ "\n")
+            # for el in row[:-1]:
+            #     f.write(str(el) + ", ")
+            f.write(str(row)+ "\n")
 
 # function for generating features from molecules
 def getfeat(smile_array):
@@ -54,25 +54,24 @@ def getfeat(smile_array):
 # Function for generationg morgan fingerprints as inputs
 def fp_factory(smile_array, length, radius):
     data_array = []
-    for smile in smile_array:
+    for i, smile in enumerate(smile_array):
+        if i % 10000 == 0:
+            print(i)
         m = Chem.MolFromSmiles(smile)
         data_array.append((AllChem.GetMorganFingerprintAsBitVect(
             m, radius, nBits=length)).ToBitString())
-    in_array = np.array(data_array)
-    out_array = []
-    for fp_string in in_array:
-        out_array.append(list(fp_string))
-    return np.array(out_array, dtype=int)
+    data_array = np.array(data_array)
+    return np.array([list(fp_string) for fp_string in data_array], dtype=int)
 
 
 print("Loading data :)")
 #store gap values
-Y_train = df_train.gap.values
-X_test = (fp_factory(df_test.smiles.values, fpl, 2))
-write_to_inp_file("X_test2.csv", X_test)
-base_X = np.vstack(fp_factory(df_train.smiles.values, fpl, 2))
-write_to_inp_file("base_X2.csv", base_X)
-base_Y = np.vstack(Y_train)
+# X_test = (fp_factory(df_test.smiles.values, fpl, 2))
+# write_to_inp_file("X_test2.csv", X_test)
+# base_X = np.vstack(fp_factory(df_train.smiles.values, fpl, 2))
+# write_to_inp_file("base_X2.csv", base_X)
+base_Y = np.vstack(df_y_train.values)
+base_X = df_train_new.values
 
 
 def ridgereg(a):
@@ -80,6 +79,8 @@ def ridgereg(a):
     clf = Ridge(alpha=a)
     clf.fit(base_X, base_Y)
     print ("Score = %f" % clf.score(base_X, base_Y))
+    df_test_new = pd.read_csv("X_test2.csv")
+    X_test = df_test_new.values
     clf_pred = clf.predict(X_test)
     write_to_file("ridge.csv", clf_pred)
 
@@ -88,6 +89,8 @@ def lassoreg(a):
     clf2 = Lasso(alpha=a)
     clf2.fit(base_X, base_Y)
     print ("Score = %f" % clf2.score(base_X, base_Y))
+    df_test_new = pd.read_csv("X_test2.csv")
+    X_test = df_test_new.values
     clf2_pred = clf2.predict(X_test)
     write_to_file("lasso.csv", clf2_pred)
 
@@ -96,6 +99,8 @@ def enet(a):
     clf3 = ElasticNet(alpha=a)
     clf3.fit(base_X, base_Y)
     print ("Score = %f" % clf3.score(base_X, base_Y))
+    df_test_new = pd.read_csv("X_test2.csv")
+    X_test = df_test_new.values
     clf3_pred = clf3.predict(X_test)
     write_to_file("elastic.csv", clf3_pred)
 
@@ -103,10 +108,10 @@ def enetCV():
     print ("Doing elastic net")
     cross_val = cross_validation.ShuffleSplit(len(base_X), n_iter=5, test_size=0.2, random_state=0)
     clf4 = ElasticNetCV(cv=cross_val)
-    print(base_X.shape)
-    print(base_Y.shape)
     clf4.fit(base_X, base_Y)
     print ("Score = %f" % clf4.score(base_X, base_Y))
+    df_test_new = pd.read_csv("X_test2.csv")
+    X_test = df_test_new.values
     clf4_pred = clf4.predict(X_test)
     write_to_file("elasticCV.csv", clf4_pred)
 
@@ -116,6 +121,8 @@ def lassolarscv():
     clf5 = LassoLarsCV(cv=cross_val)
     clf5.fit(base_X, base_Y)
     print ("Score = %f" % clf5.score(base_X, base_Y))
+    df_test_new = pd.read_csv("X_test2.csv")
+    X_test = df_test_new.values
     clf5_pred = clf5.predict(X_test)
     write_to_file("lassolars.csv", clf5_pred)
 

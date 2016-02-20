29419b963ffa3ff93318521457ddce539721d03e
Abby <abigaillyons@college.harvard.edu>
2016-02-20 18:15:25 -0500

    fixed edge count problem

--- 124programming1/prim.cpp
+++ 124programming1/prim.cpp
@@ -14,20 +14,21 @@ bool done(bool * array, unsigned int size)
 double prim(Graph g)
 {
 	// initialize priority queue
-	unsigned int size = g.size();
-	Pqueue pqueue(size);
+	unsigned int vertices = g.size();
 	std::vector<Edge> edges = g.getEdges();
+	unsigned int edgeCount = edges.size();
+	Pqueue pqueue(edgeCount);
 
 	// initialize finished array
-	bool finished[size];
-	memset(finished, size * sizeof(bool), 0);
+	bool finished[vertices];
+	memset(finished, vertices * sizeof(bool), 0);
 	finished[edges[0].a] = true;
 
 	double cost = 0.0;
 	// start iterating
-	while (!done(finished, size))
+	while (!done(finished, vertices))
 	{
-		for (unsigned int i = 0; i < size; ++i)
+		for (unsigned int i = 0; i < edgeCount; ++i)
 		{
 			if (finished[edges[i].b])  continue;
 			if (finished[edges[i].a])  pqueue.insert(edges[i]);

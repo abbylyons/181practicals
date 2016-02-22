3dea2b3c53781de3dd4a6e9f22a3b68b2d8dc6dc
Timothy tamm <tamm@college.harvard.edu>
2016-02-22 00:26:14 -0500

    Ich bin ein scriptkinder

---
+++ 124programming1/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+declare -a numpoints=(16 32 64 128 256 512 1024 2048 4096 8192 16384 32768 65536)
+for i in `seq 0 4`;
+do
+  if [  "$i" -ne "1"  ]
+    then
+      for n in `seq 0 11`;
+      do
+        ./randmst 0 ${numpoints[$n]} 5 $i
+      done
+  fi
+done

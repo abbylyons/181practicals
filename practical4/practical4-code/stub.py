ac9f9f9156733c06b601963ebffc2b4e9632456f
Timothy tamm <tamm@college.harvard.edu>
2016-04-28 20:00:49 -0400

    push

--- practical4/practical4-code/stub.py
+++ practical4/practical4-code/stub.py
@@ -2,6 +2,7 @@
 import numpy as np
 import numpy.random as npr
 import neural_net.data as nn
+import random
 
 from SwingyMonkey import SwingyMonkey
 
@@ -17,12 +18,13 @@ class Learner(object):
         self.last_reward = None
         self.impulse = 15
         # [vertical_dist][horz_dist][grav][action]
-        self.Qs = np.array([[[[0 for _ in range(2)] for _ in range(2)] for _ in range(10)] for _ in range(12)])
+        self.Qs = np.array([[[[0 for _ in range(2)] for _ in range(2)] for _ in range(20)] for _ in range(21)])
         self.gamma = .7
         self.grav = None
         self.horz_speed = None
         self.first_round = True
         self.tree_gap = None
+        self.iters = 1
 
     def reset(self):
         self.last_state  = None
@@ -60,27 +62,36 @@ class Learner(object):
         else:
             #update Q values for the last iteration
             # figure out the previous bins:
-            prev_v_dist = (self.last_state['tree']['top'] - self.last_state['monkey']['top']+ self.tree_gap)/50
-            prev_horz_dist = (self.last_state['tree']['dist']+150)/100
+            prev_v_dist = (self.last_state['tree']['top'] - self.last_state['monkey']['top']+ self.tree_gap)/31
+            prev_horz_dist = (self.last_state['tree']['dist']+150)/50
             # figure out the bins:
-            v_dist = (state['tree']['top'] - state['monkey']['top']+self.tree_gap)/50
-            horz_dist = (state['tree']['dist']+150)/100
+            v_dist = (state['tree']['top'] - state['monkey']['top']+self.tree_gap)/31
+            horz_dist = (state['tree']['dist']+150)/50
             self.Qs[prev_v_dist][prev_horz_dist][self.gravity][self.last_action] = self.last_reward + self.gamma * max(self.Qs[v_dist][horz_dist][0])
             action0Q = self.Qs[v_dist][horz_dist][0][0]
             action1Q = self.Qs[v_dist][horz_dist][0][1]
 
 
             # this can be better
-            if action0Q == 0:
+            prob = np.exp(self.iters*action1Q/100)
+            prob = prob/(prob+np.exp(self.iters*action0Q/100))
+            if random.random() > prob:
                 new_action = 0
-            elif action1Q == 0:
-                new_action = 1;
             else:
-                new_action = self.Qs[v_dist][horz_dist][self.gravity].argmax()
+                new_action = 1
+
+            # if action0Q == 0:
+            #     new_action = 0
+            # elif action0Q == 0:
+            #     new_action = 1
+            # else:
+            #     new_action = self.Qs[v_dist][horz_dist][0].argmax()
+
             new_state  = state
 
         self.last_action = new_action
         self.last_state  = new_state
+        self.iters += 1
 
         return self.last_action
 
